3d06926970bddaca2894f5a097bc9303986664f8
JHANFRAVAR <mskartel@gmail.com>
2014-10-24 12:31:26 -0500

    Vigenere
    Cifrado Vigenere

---
+++ src/Vigenere.java
@@ -0,0 +1,33 @@
+public class Vigenere {
+    public static void main(String[] args) {
+        String key = "VIGENERE";
+        String ori = "ELECTIVA PROFESIONAL III";
+        String enc = encrypt(ori, key);
+        System.out.println(enc);
+        System.out.println(decrypt(enc, key));
+    }
+ 
+    static String encrypt(String text, final String key) {
+        String res = "";
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z') continue;
+            res += (char)((c + key.charAt(j) - 2 * 'A') % 26 + 'A');
+            j = ++j % key.length();
+        }
+        return res;
+    }
+ 
+    static String decrypt(String text, final String key) {
+        String res = "";
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z') continue;
+            res += (char)((c - key.charAt(j) + 26) % 26 + 'A');
+            j = ++j % key.length();
+        }
+        return res;
+    }
+}
